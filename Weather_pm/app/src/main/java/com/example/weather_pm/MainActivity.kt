cdc36dafc648a5a514d1a7273a3ec8af5ec8f6e3
imasyalol <pahmurinmax@gmail.com>
2020-12-26 11:05:23 +0300

    lab 8 Pahmurin

--- Weather_pm/app/src/main/java/com/example/weather_pm/MainActivity.kt
+++ Weather_pm/app/src/main/java/com/example/weather_pm/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.weather_pm
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.squareup.moshi.Json
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,12 +14,13 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import kotlin.math.roundToInt
+
 
 class WeatherResponse(
-    @field:Json(name = "lat") var lat: Float,
-    @field:Json(name = "lon") var lon: Float,
     @field:Json(name = "timezone") var timezone: String,
-    @field:Json(name = "current") var current: Current
+    @field:Json(name = "current") var current: Current,
+    @field:Json(name = "daily") var daily: List<Daily>
 )
 
 class Current(
@@ -25,59 +28,117 @@ class Current(
     @field:Json(name = "temp") var temp: Float,
     @field:Json(name = "clouds") var clouds: Int,
     @field:Json(name = "wind_speed") var wind_speed: Int,
-    @field:Json(name = "pressure") var pressure: Int
+    @field:Json(name = "weather") var weather: List<WeatherDescription>
+)
 
+class Daily(
+    @field:Json(name = "temp") var temp: Temp,
+    @field:Json(name = "weather") var weather: List<WeatherDescription>
+)
+
+class Temp(
+    @field:Json(name = "day") var day: Float
+)
+
+class WeatherDescription(
+    @field:Json(name = "main") var main: String,
+    @field:Json(name = "icon") var icon: String
 )
 
 
 
 @Suppress("UNSAFE_CALL_ON_PARTIALLY_DEFINED_RESOURCE")
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     companion object{
         const val URL = "https://api.openweathermap.org/data/2.5/"
-        const val lat = "59.937500"
-        const val lon = "30.308611"
-        const val exclude = "minutely,hourly,daily,alerts"
-        const val API_KEY = "6f102fd88763d69f0b8347b981501367"
     }
 
-    public interface OpenWeatherMapService {
-        @GET("onecall?lat=59.937500&lon=30.308611&exclude=minutely,hourly,alerts&units=metric&appid=6f102fd88763d69f0b8347b981501367")
+    interface OpenWeatherMapService {
+        @GET("onecall?lat=59.937500&lon=30.308611&exclude=minutely,hourly,alerts&units=metric&appid=508af3c89d5fdcb4b9f030cea26e964e")
         fun listWeather(): Call<WeatherResponse>
     }
 
     private var flagTheme = false
+    private val retrofit =
+        Retrofit.Builder()
+            .baseUrl(URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    private val service = retrofit.create(OpenWeatherMapService::class.java)
+    private var call: Call<WeatherResponse>? = service.listWeather()
 
+    @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val retrofit =
-            Retrofit.Builder()
-                .baseUrl(URL)
-                .addConverterFactory(MoshiConverterFactory.create())
-                .build()
-        val service = retrofit.create(OpenWeatherMapService::class.java)
-        val call: Call<WeatherResponse> = service.listWeather()
-        call.enqueue(object : Callback<WeatherResponse> {
+
+        val prefs = getSharedPreferences("storage", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+
+        fun setViewInfo() {
+            textView.text = prefs.getString("textView", "")!!
+            textViewWindFLow.text = prefs.getString("textViewWindFLow", "")!!
+            textViewCloudy.text = prefs.getString("textViewCloudy", "")!!
+            textViewHumidity.text = prefs.getString("textViewHumidity", "")!!
+            textViewTempPN.text = prefs.getString("textViewTempPN", "")!!
+            textViewTempVT.text = prefs.getString("textViewTempVT", "")!!
+            textViewTempSR.text = prefs.getString("textViewTempSR", "")!!
+            textViewTempCT.text = prefs.getString("textViewTempCT", "")!!
+            textViewTempPT.text = prefs.getString("textViewTempPT", "")!!
+            textViewTempSB.text = prefs.getString("textViewTempSB", "")!!
+            textViewTempVS.text = prefs.getString("textViewTempVS", "")!!
+        }
+
+
+        call?.enqueue(object : Callback<WeatherResponse> {
+            @SuppressLint("SetTextI18n")
             override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
                 if (response.code() == 200) {
                     val weatherResponse = response.body()!!
-
-                    val stringBuilder = "lat: " +
-                            weatherResponse.lat.toString() +
-                            "\n"
-
-                    Log.d("MSG-ff", stringBuilder)
-                    temperature.text = weatherResponse.current.temp.toString()
-                    pressure.text = weatherResponse.current.pressure.toString()
-                    pressure.text = weatherResponse.current.pressure.toString()
-                    WindFLow.text = weatherResponse.current.wind_speed.toString()
-                    humidity.text = weatherResponse.current.humidity.toString()
+                    editor.putInt("textView", weatherResponse.current.temp.roundToInt()).apply()
+                    val t = weatherResponse.current.temp.roundToInt()
+                    if (t > 0) {
+                        editor.putString("textView", "${t}°C").apply()
+                    } else {
+                        editor.putString("textView", "${t}°C").apply()
+                    }
+                    editor.putString("textView2", weatherResponse.timezone + "\n" + weatherResponse.current.weather[0].main).apply()
+                    editor.putString("textViewWindFLow", weatherResponse.current.wind_speed.toString()).apply()
+                    editor.putString("textViewCloudy", weatherResponse.current.clouds.toString()).apply()
+                    editor.putString("textViewHumidity", weatherResponse.current.humidity.toString()).apply()
+                    val icon1 = weatherResponse.current.weather[0].icon
+                    Picasso.get()
+                        .load("https://openweathermap.org/img/wn/$icon1@2x.png")
+                        .fit().centerCrop()
+                        .into(icon)
+                    fun genSaveEveryDay(days: List<Daily>) {
+                        val textViewTempDay = listOf(
+                            "textViewTempPN",
+                            "textViewTempVT",
+                            "textViewTempSR",
+                            "textViewTempCT",
+                            "textViewTempPT",
+                            "textViewTempSB",
+                            "textViewTempVS"
+                        )
+                        days.forEachIndexed { index, day ->
+                            if (index != 0) {
+                                val tempNow = day.temp.day.roundToInt()
+                                if (tempNow >= 0) {
+                                    editor.putString(textViewTempDay[index - 1], "$tempNow").apply()
+                                } else {
+                                    editor.putString(textViewTempDay[index - 1], "$tempNow").apply()
+                                }
+                            }
+                        }
+                    }
+                    genSaveEveryDay(weatherResponse.daily)
                 }
+                setViewInfo()
             }
 
             override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                Log.d("MSG-ff", "ERROR")
+                setViewInfo()
             }
         })
     }
@@ -86,6 +147,12 @@ class MainActivity : Activity() {
         super.onSaveInstanceState(outState)
 
         outState.putBoolean("flagTheme", flagTheme)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        call?.cancel()
+        call = null
     }
 
 }
